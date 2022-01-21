9e0fd25f36446fde1a32f7d26e4191de19eabd9c
“davidtoris” <edavid.toris@gmail.com>
2022-01-21 12:45:51 -0600

    add filter svg

--- src/components/filter/index.jsx
+++ src/components/filter/index.jsx
@@ -8,8 +8,8 @@ const Filter = ({specialities : { detalle }}) => {
       <form>
         <div className='text-righ flex justify-end'>
           <div className='text-blueDarkCustom mr-2 font-semibold text-lg flex items-center'>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" />
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clipRule="evenodd" />
             </svg>
             Filtrar por:
           </div> 
