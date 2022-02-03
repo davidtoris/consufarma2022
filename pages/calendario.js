24dde79ca2e79d8f1caeaba9e6713ec1e74d66ea
“davidtoris” <edavid.toris@gmail.com>
2022-02-02 21:52:27 -0600

    quitar variable print

--- pages/calendario.js
+++ pages/calendario.js
@@ -1,14 +1,9 @@
-import Head from 'next/head';
-import React from 'react';
 import NavBar from '../src/components/NavBar';
 
 
 const calendario = () => {
   return (
     <>
-    <Head>
-      <link rel="StyleSheet" href={print} type="text/css" />
-    </Head>
       <NavBar />
       <div className='print'>Calendario</div>
     </>
