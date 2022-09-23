07d213cbf73ec68dad84e5cec5545fd0d985002d
“davidtoris” <edavid.toris@gmail.com>
2022-09-23 09:10:20 -0500

    add calendar styles

--- pages/cursos/calendario.js
+++ pages/cursos/calendario.js
@@ -14,83 +14,115 @@ const Calendar = ({cursosDate}) => {
 
 
   return (
-    <>
-      <NavBar />
-      <div className='w-6/12 m-auto'>
-        <img src='../logo.png' alt='' />
-      </div>
-      <div className='w-12/12'>
-        <Image src="/calendario/banner1.png" objectFit='content' width={2200} height={150} />
-      </div>
+    <div className=''>
+      <div className=''>
+        <div className='w-[1000px] m-auto'>
+          <div className='w-12/12'>
+            <Image src="/calendario/banner1.png" objectFit='content' width={1000} height={250} />
+          </div>
 
-      <div className='container m-auto px-2 md:px-2 mt-10 cont-cols-calendar'>
+          <div className='container m-auto px-2 md:px-2 mt-0 cont-cols-calendar'>
 
-        <div>
-          <h2 className='text-4xl text-blueDarkCustom m-auto mt-6 font-bold text-center mb-8'>Septiembre</h2>
-          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 cols-calendar'>
-            {cursosDate.filter(f => f.fecha_text.includes('Septiembre')).map(c => (
-              <CardCalendar
-                key={c._id}
-                nombre={c.nombre}
-                fecha={c.fecha_text}
-                duracion={c.duracion}
-               />
-            ))}
+            <div>
+              <h2 className='text-4xl text-blueDarkCustom m-auto mt-2 font-bold text-center mb-8 uppercase'>Septiembre</h2>
+              <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 cols-calendar'>
+                {cursosDate.filter(f => f.fecha_text.includes('Septiembre')).map(c => (
+                  <CardCalendar
+                    key={c._id}
+                    nombre={c.nombre}
+                    fecha={c.fecha_text}
+                    duracion={c.duracion}
+                    horario={c.horario}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+
+          <div className='h-[63px]'></div>
+          <div className='w-12/12'>
+            <Image src="/calendario/footer.png" objectFit='content' width={1000} height={40} />
           </div>
         </div>
       </div>
 
-      <div className='w-12/12'>
-        <Image src="/calendario/banner1.png" objectFit='content' width={2200} height={150} />
-      </div>
 
-      <div className='container m-auto px-2 md:px-2 mt-10 cont-cols-calendar'>
-        
-        <div>
-          <h2 className='text-4xl text-blueDarkCustom m-auto mt-6 font-bold text-center mb-8'>Octubre</h2>
-          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-            {cursosDate.filter(f => f.fecha_text.includes('Octubre')).map(c => (
-              <CardCalendar
-                key={c._id}
-                nombre={c.nombre}
-                fecha={c.fecha_text}
-                duracion={c.duracion}
-               />
-            ))}
+      <div className=''>
+        <div className='w-[1000px] m-auto'>
+          <div className='w-12/12'>
+            <Image src="/calendario/banner2.png" objectFit='content' width={1000} height={250} />
           </div>
-        </div>
-       
-        <div>
-          <h2 className='text-4xl text-blueDarkCustom m-auto mt-6 font-bold text-center mb-8'>Noviembre</h2>
-          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-            {cursosDate.filter(f => f.fecha_text.includes('Noviembre')).map(c => (
-              <CardCalendar
-                key={c._id}
-                nombre={c.nombre}
-                fecha={c.fecha_text}
-                duracion={c.duracion}
-               />
-            ))}
-          </div>
-        </div>
-       
-        <div>
-          <h2 className='text-4xl text-blueDarkCustom m-auto mt-6 font-bold text-center mb-8'>Diciembre</h2>
-          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-            {cursosDate.filter(f => f.fecha_text.includes('Diciembre')).map(c => (
-              <CardCalendar
-                key={c._id}
-                nombre={c.nombre}
-                fecha={c.fecha_text}
-                duracion={c.duracion}
-               />
-            ))}
-          </div>
-        </div>
 
+          <div className='container m-auto px-2 md:px-2 mt-0 cont-cols-calendar'>
+
+            <div>
+              <h2 className='text-4xl text-blueDarkCustom m-auto mt-6 font-bold text-center mb-8 uppercase'>Octubre</h2>
+              <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 cols-calendar'>
+                {cursosDate.filter(f => f.fecha_text.includes('Octubre')).map(c => (
+                  <CardCalendar
+                    key={c._id}
+                    nombre={c.nombre}
+                    fecha={c.fecha_text}
+                    duracion={c.duracion}
+                    horario={c.horario}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+
+          <div className='h-[43px]'></div>
+          <div className='w-12/12'>
+            <Image src="/calendario/footer.png" objectFit='content' width={1000} height={40} />
+          </div>
+        </div>
       </div>
-      <Footer />
-    </>
+      
+      
+      <div className=''>
+        <div className='w-[1000px] m-auto'>
+          <div className='w-12/12'>
+            <Image src="/calendario/banner3.png" objectFit='content' width={1000} height={250} />
+          </div>
+
+          <div className='container m-auto px-2 md:px-2 mt-0 cont-cols-calendar'>
+
+            <div>
+              <h2 className='text-4xl text-blueDarkCustom m-auto mt-6 font-bold text-center mb-8 uppercase'>Noviembre y Diciembre</h2>
+              <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 cols-calendar'>
+                {cursosDate.filter(f => f.fecha_text.includes('Noviembre')).map(c => (
+                  <CardCalendar
+                    key={c._id}
+                    nombre={c.nombre}
+                    fecha={c.fecha_text}
+                    duracion={c.duracion}
+                    horario={c.horario}
+                  />
+                ))}
+              </div>
+            </div>
+            <div>
+              <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 cols-calendar'>
+                {cursosDate.filter(f => f.fecha_text.includes('Diciembre')).map(c => (
+                  <CardCalendar
+                    key={c._id}
+                    nombre={c.nombre}
+                    fecha={c.fecha_text}
+                    duracion={c.duracion}
+                    horario={c.horario}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+
+          <div className='h-[43px]'></div>
+          <div className='w-12/12'>
+            <Image src="/calendario/footer.png" objectFit='content' width={1000} height={40} />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 };
 
--- src/components/cursos/cardCalendar.js
+++ src/components/cursos/cardCalendar.js
@@ -1,21 +1,29 @@
 import React from 'react'
+import { FaCalendarAlt, FaClock } from 'react-icons/fa';
 
-const CardCalendar = ({ nombre, fecha, duracion}) => {
+const CardCalendar = ({ nombre, fecha, duracion, horario}) => {
   return (
     <>
-      <div className='hover:scale-105 transition transform duration-200 ease-out p-5 pt-3 font-body
-      shadow-lg bg-white rounded-md w-[320px] shrink-0 cursor-pointer mb-7 h-[180px] flex align-middle justify-center card-calendar'>
+      <div className='hover:scale-105 transition transform duration-200 ease-out px-0 py-3 font-body
+      shadow-xl bg-white rounded-md w-[320px] shrink-0 cursor-pointer mb-4 h-[280px] flex align-middle justify-center card-calendar border-solid border-2 border-gray-200'>
 
-        <div>
-          <div className='name-course font-semibold text-gray-700 text-center'>
+        <div className=''>
+          <div className='name-course font-semibold text-gray-700 text-center text-base p-2'>
             {nombre}
           </div>
-          <div className='name-course font-semibold text-center bg-blueLightCustom w-8/12 m-auto mt-2 text-white rounded-md'>
-            {fecha}
+          <div className='name-course font-semibold text-center w-full m-auto mt-4 pt-1 pb-3 text-blueConsufarma text-sm'>
+          <div className='font-bold mt-1 flex justify-center items-center'>
+                <FaCalendarAlt className='w-4 h-4 ml-3 mr-1'/>
+                {fecha}
+              </div>
+              <div className='font-bold mt-1 flex justify-center items-center'>
+                <FaClock className='w-4 h-4 ml-1 mr-1'/>
+                {duracion} {horario}
+              </div>
           </div>
-          {/* <div className='name-course font-semibold text-gray-700 text-center'>
-            {duracion}
-          </div> */}
+          <div className='bg-blueLightCustom text-white text-center font-bold p-2 text-sm m-3 rounded-md'>
+            ONLINE
+          </div>
         </div>
         
       </div>
