6cf09c0b3191fff67cde279946b238b72443f853
“davidtoris” <edavid.toris@gmail.com>
2022-12-28 07:14:16 -0600

    comp: add url production

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
-// export const API_BASE_URL = 'https://consu-back-node.herokuapp.com/api';
-export const API_BASE_URL = 'http://localhost:3000/api';
+export const API_BASE_URL = 'https://consufarma2023-61f76kviz-davidtoris.vercel.app/';
+// export const API_BASE_URL = 'http://localhost:3000/api';
 // export const API_BASE_URL = 'http://localhost:8080/api';
