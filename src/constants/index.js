9cf308c7443d5ec52750969bcc6bc5e74a38d13e
“davidtoris” <edavid.toris@gmail.com>
2022-12-29 12:27:49 -0600

    add const production

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,2 @@
 // export const API_BASE_URL = 'https://consu-back-node.herokuapp.com/api';
 export const API_BASE_URL = 'https://consu-back-node-production.up.railway.app/api';
-// export const API_BASE_URL = 'http://localhost:8080/api';
