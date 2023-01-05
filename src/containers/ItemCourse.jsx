d0ef7d55c0585882e5d79bc2450327807c035036
“davidtoris” <edavid.toris@gmail.com>
2023-01-05 11:16:01 -0600

    imagae

--- src/containers/ItemCourse.jsx
+++ src/containers/ItemCourse.jsx
@@ -161,9 +161,11 @@ const ItemCourse = ({curso}) => {
         </div>
       </div>
       <div className='max-w-7xl container mx-auto '>
-        <div className='ponente bg-blueConsufarma text-white my-8 flex p-4 pb-5 rounded-md w-12/12'>
+        <div className='ponente bg-blueConsufarma text-white my-8 sm:block md:flex p-4 pb-5 rounded-md w-12/12'>
           {ponente_img && (
-            <Image src={ponente_img} layout='fixed' width={250} height={90} alt="" />
+            <>
+              <img src={ponente_img} layout='fixed' width="20%" alt="" className="mb-2" />
+            </>
           )}
           <div className='ml-5'>
             <h2 className='text-2xl font-bold mb-2'>{ponente}</h2>
