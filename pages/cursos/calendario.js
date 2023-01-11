9c1bb33f1862ed6cbc6cbdeb545c0af84b26aa9b
“davidtoris” <edavid.toris@gmail.com>
2023-01-11 14:04:07 -0600

    add months in claendar

--- pages/cursos/calendario.js
+++ pages/cursos/calendario.js
@@ -131,6 +131,102 @@ const Calendar = ({cursosDate}) => {
           </div> */}
         </div>
       </div>
+      
+      <div className=''>
+        <div className='w-[1000px] m-auto'>
+          <div className='w-12/12'>
+            <Image src="/calendario/banner3.png" objectFit='content' width={1000} height={250} />
+          </div>
+
+          <div className='container m-auto px-2 md:px-2 mt-0 cont-cols-calendar'>
+
+            <div>
+              <h2 className='text-4xl text-blueDarkCustom m-auto mt-6 font-bold text-center mb-8 uppercase'>Mayo</h2>
+              <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 cols-calendar'>
+                {cursosDate.filter(f => f.fecha_text.includes('Mayo')).map(c => (
+                  <CardCalendar
+                    key={c._id}
+                    nombre={c.nombre}
+                    fecha={c.fecha_text}
+                    duracion={c.duracion}
+                    horario={c.horario}
+                    link={c.nombre_ruta}
+                    color="blueLightCustom"
+                  />
+                ))}
+              </div>
+            </div>
+            {/* <div>
+              <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 cols-calendar'>
+                {cursosDate.filter(f => f.fecha_text.includes('Diciembre')).map(c => (
+                  <CardCalendar
+                    key={c._id}
+                    nombre={c.nombre}
+                    fecha={c.fecha_text}
+                    duracion={c.duracion}
+                    horario={c.horario}
+                    link={c.nombre_ruta}
+                    color="redConsufarma"
+                  />
+                ))}
+              </div>
+            </div> */}
+          </div>
+
+          <div className='h-[43px]'></div>
+          {/* <div className='w-12/12'>
+            <Image src="/calendario/footer.png" objectFit='content' width={1000} height={40} />
+          </div> */}
+        </div>
+      </div>
+      
+      <div className=''>
+        <div className='w-[1000px] m-auto'>
+          <div className='w-12/12'>
+            <Image src="/calendario/banner3.png" objectFit='content' width={1000} height={250} />
+          </div>
+
+          <div className='container m-auto px-2 md:px-2 mt-0 cont-cols-calendar'>
+
+            <div>
+              <h2 className='text-4xl text-blueDarkCustom m-auto mt-6 font-bold text-center mb-8 uppercase'>Junio</h2>
+              <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 cols-calendar'>
+                {cursosDate.filter(f => f.fecha_text.includes('Junio')).map(c => (
+                  <CardCalendar
+                    key={c._id}
+                    nombre={c.nombre}
+                    fecha={c.fecha_text}
+                    duracion={c.duracion}
+                    horario={c.horario}
+                    link={c.nombre_ruta}
+                    color="blueConsufarma"
+                  />
+                ))}
+              </div>
+            </div>
+            {/* <div>
+              <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 cols-calendar'>
+                {cursosDate.filter(f => f.fecha_text.includes('Diciembre')).map(c => (
+                  <CardCalendar
+                    key={c._id}
+                    nombre={c.nombre}
+                    fecha={c.fecha_text}
+                    duracion={c.duracion}
+                    horario={c.horario}
+                    link={c.nombre_ruta}
+                    color="redConsufarma"
+                  />
+                ))}
+              </div>
+            </div> */}
+          </div>
+
+          <div className='h-[43px]'></div>
+          {/* <div className='w-12/12'>
+            <Image src="/calendario/footer.png" objectFit='content' width={1000} height={40} />
+          </div> */}
+        </div>
+      </div>
     </div>
   )
 };
