efa18f7f8c26948c554285c2f1746421ee37b2b9
“davidtoris” <edavid.toris@gmail.com>
2023-01-23 12:28:47 -0600

    better mobile content filter

--- src/components/filter/index.jsx
+++ src/components/filter/index.jsx
@@ -39,28 +39,41 @@ const Filter = ({specialities, active}) => {
                   Especialidad
               </div>
             </Link>
-          </div>
-          
-          <form>
-            <div className='text-righ flex justify-end pr-4 xs:pr-0 mt-3 md:mt-0'>
-              <div className='text-white mr-2 font-semibold text-lg flex items-center'>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                  <path fillRule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clipRule="evenodd" />
-                </svg>
-                <span className='hidden md:inline'>Filtrar por especialidad:</span>
-                <span className='inline md:hidden'>Especialidad</span>
-              </div> 
-              <select name="especialidad" className='bg-white p-2 text-blueConsufarma rounded-md pr-0' onChange={handleChange}>
-                  <option value="" className='text-blueConsufarma'>Todas</option>
-                {specialities.map(s => (
-                  <option 
-                    value={s.especialidad} 
-                    key={s._id}
-                    >{s.especialidad}</option>
-                  ))}
+            <div className='ml-2 text-white relative inline md:hidden'>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clipRule="evenodd" />
+              </svg>
+              <select name="especialidad" className='bg-white w-1/12 opacity-0 absolute' onChange={handleChange}>
+                <option value="" className='text-blueConsufarma'>Todas</option>
+              {specialities.map(s => (
+                <option 
+                  value={s.especialidad} 
+                  key={s._id}
+                  >{s.especialidad}</option>
+                ))}
               </select>
             </div>
-          </form>
+            
+          </div>
+          
+          <div className='text-righ justify-end pr-4 xs:pr-0 mt-3 md:mt-0 hidden md:flex'>
+            <div className='text-white mr-2 font-semibold text-lg flex items-center'>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clipRule="evenodd" />
+              </svg>
+              <span className='hidden md:inline'>Filtrar por especialidad:</span>
+              <span className='inline md:hidden'>Especialidad</span>
+            </div> 
+            <select name="especialidad" className='bg-white p-2 text-blueConsufarma rounded-md pr-0' onChange={handleChange}>
+                <option value="" className='text-blueConsufarma'>Todas</option>
+              {specialities.map(s => (
+                <option 
+                  value={s.especialidad} 
+                  key={s._id}
+                  >{s.especialidad}</option>
+                ))}
+            </select>
+          </div>
           
       </section>
       </div>
