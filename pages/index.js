09f43abc388633e95ebbb5e99c60437c043936f3
“davidtoris” <edavid.toris@gmail.com>
2023-04-18 13:54:34 -0600

    hide modal

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ const Calendario = ({specialities, cursosDate}) => {
         active='fecha'
       />
       
-      <Modal />
+      {/* <Modal /> */}
 
       <div className='container m-auto px-2 md:px-2 mt-10'>
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
