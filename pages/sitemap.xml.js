519c1206d94b02c0cfc5fe02f9e51a96d94a28b1
“davidtoris” <edavid.toris@gmail.com>
2023-06-03 14:57:15 -0600

    add sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,53 @@
+const EXTERNAL_DATA_URL = 'https://consu-back-node.vercel.app/api/courses';
+const URL = 'https://consufarma.com';
+
+import React from 'react'
+
+function generateSiteMap(posts) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>https://jsonplaceholder.typicode.com</loc>
+     </url>
+     <url>
+       <loc>https://jsonplaceholder.typicode.com/guide</loc>
+     </url>
+     ${posts
+       .map(({ nombre_ruta }) => {
+         return `
+       <url>
+           <loc>${`${URL}/cursos/${nombre_ruta}`}</loc>
+       </url>
+     `;
+       })
+       .join('')}
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }) {
+  // We make an API call to gather the URLs for our site
+  const request = await fetch(EXTERNAL_DATA_URL);
+  const posts = await request.json();
+
+  const postArray = posts.courses
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(postArray);
+
+  res.setHeader('Content-Type', 'text/xml');
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
