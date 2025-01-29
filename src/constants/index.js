4beb26bfc042db11f74576a7f37dd68b6a95fe80
davidtoris <edavid.toris@gmail.com>
2025-01-28 18:30:08 -0600

    url site

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,5 @@
 export const API_BASE_URL = 'https://consu-back-node.vercel.app/api';
+export const URL_SITE = 'https://consufarma.com';
+
 // export const API_BASE_URL = 'http://localhost:8080/api';
+// export const URL_SITE = 'http://localhost:3000';
